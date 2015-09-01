5b63b76f0da2eef4be7f4ab44f00ab969735c9b0
Or Ami <ora@mellanox.com>
2015-09-01 15:23:55 +0300

    mlx5: fix element array size declaration
    Although it doesn't seem to cause issues, rxq->elts_m is 0 since the code
    was moved in the following commit.
    
    Fixes: baa51f1b3837 ("mlx5: get rid of the WR structure in RX queue elements")
    
    Signed-off-by: Or Ami <ora@mellanox.com>
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>
    Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

--- drivers/net/mlx5/mlx5_rxq.c
+++ drivers/net/mlx5/mlx5_rxq.c
@@ -979,7 +979,7 @@ rxq_rehash(struct rte_eth_dev *dev, struct rxq *rxq)
 	/* Post SGEs. */
 	assert(tmpl.if_wq != NULL);
 	if (tmpl.sp) {
-		struct rxq_elt_sp (*elts)[rxq->elts_n] = tmpl.elts.sp;
+		struct rxq_elt_sp (*elts)[tmpl.elts_n] = tmpl.elts.sp;
 
 		for (i = 0; (i != elemof(*elts)); ++i) {
 			err = tmpl.if_wq->recv_sg_list
@@ -990,7 +990,7 @@ rxq_rehash(struct rte_eth_dev *dev, struct rxq *rxq)
 				break;
 		}
 	} else {
-		struct rxq_elt (*elts)[rxq->elts_n] = tmpl.elts.no_sp;
+		struct rxq_elt (*elts)[tmpl.elts_n] = tmpl.elts.no_sp;
 
 		for (i = 0; (i != elemof(*elts)); ++i) {
 			err = tmpl.if_wq->recv_burst(
