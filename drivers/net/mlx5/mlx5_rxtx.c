serglelv/dpdk.org

commit d85709205b91f8b1f808465b053ef7f37b7f3247

mlx5: add missing features
1.add hardware RX checksum offload for L3/L4 packets.
2.add vlan stripping.
3.add vlan insertion.

Signed-off-by: Yaacov Hazan <yaacovh@mellanox.com>

drivers/net/mlx5/mlx5_rxtx.c
@@ -384,7 +384,6 @@ static inline void
 mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	       uintptr_t addr, uint32_t length, uint32_t lkey)
 {
-
 	/* Copy the first 16 bytes into the inline header */
 	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start,
 	       (void *)(uintptr_t)addr,
@@ -404,18 +403,88 @@ mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	++txq->wqe_ci;
 }
 
+static inline void
+mlx5_wqe_write_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
+	       uintptr_t addr, uint32_t length, uint32_t lkey,
+	       uint16_t *vlan_tci)
+{
+	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+
+	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
+	/*
+	 * Copy 12 bytes of source & destination MAC address.
+	 * Copy 4 bytes of vlan.
+	 * Copy 2 bytes of ether type.
+	 */
+	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start, (void *)(uintptr_t)addr, 12);
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 12), &vlan, sizeof(vlan));
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 16), (void *)((uintptr_t)addr + 12), 2);
+
+	addr += MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
+	length -= MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
+
+	wqe->dseg.byte_count = htonl(length);
+	wqe->dseg.lkey = lkey;
+	wqe->dseg.addr = htonll(addr);
+
+	/* Write only data[0] which is the single element which changes.
+	 * Other fields are already initialised in txq_alloc_elts. */
+	wqe->ctrl.data[0] = htonl((txq->wqe_ci << 8) | MLX5_OPCODE_SEND);
+
+	/* Increase the consumer index. */
+	++txq->wqe_ci;
+}
+
 #else /* MLX5_PMD_MAX_INLINE == 0 */
 
 static inline void
 mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	       uintptr_t addr, uint32_t length, uint32_t lkey)
 {
+	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_INLINE_HEADER_SIZE);
 	/* Copy the first 16 bytes into the inline header */
 	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start,
 	       (void *)(uintptr_t)addr,
 	       MLX5_ETH_INLINE_HEADER_SIZE);
 	addr += MLX5_ETH_INLINE_HEADER_SIZE;
 	length -= MLX5_ETH_INLINE_HEADER_SIZE;
+
+	wqe->dseg.byte_count = htonl(length);
+	wqe->dseg.lkey = lkey;
+	wqe->dseg.addr = htonll(addr);
+
+	wqe->ctrl.data[0] = htonl((txq->wqe_ci << 8) | MLX5_OPCODE_SEND);
+	wqe->ctrl.data[1] = htonl((txq->qp_num << 8) | 4);
+	if (unlikely(--txq->elts_comp == 0)) {
+		wqe->ctrl.data[2] = htonl(8);
+		txq->elts_comp = txq->elts_comp_cd_init;
+	} else
+		wqe->ctrl.data[2] = htonl(0);
+	wqe->ctrl.data[3] = 0;
+
+	/* Increase the consumer index. */
+	++txq->wqe_ci;
+}
+
+static inline void
+mlx5_wqe_write_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
+	       uintptr_t addr, uint32_t length, uint32_t lkey,
+	       uint16_t *vlan_tci)
+{
+	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+
+	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
+	/*
+	 * Copy 12 bytes of source & destination MAC address.
+	 * Copy 4 bytes of vlan.
+	 * Copy 2 bytes of ether type.
+	 */
+	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start, (void *)(uintptr_t)addr, 12);
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 12), &vlan, sizeof(vlan));
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 16), (void *)((uintptr_t)addr + 12), 2);
+
+	addr += MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
+	length -= MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
 
 	wqe->dseg.byte_count = htonl(length);
 	wqe->dseg.lkey = lkey;
@@ -450,6 +519,69 @@ mlx5_wqe_write_inline(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 	       MLX5_ETH_INLINE_HEADER_SIZE);
 	addr += MLX5_ETH_INLINE_HEADER_SIZE;
 	length -= MLX5_ETH_INLINE_HEADER_SIZE;
+
+	size = (sizeof(wqe->ctrl.ctrl) +
+		sizeof(wqe->eseg) +
+		sizeof(wqe->dseg.byte_count) +
+		length + 15) / 16;
+
+	wqe->dseg.byte_count = htonl(length | MLX5_INLINE_SEG);
+	memcpy((void *)(uintptr_t)&inl_wqe->wqe.data[MLX5_WQE64_INL_DATA_OFFSET],
+	       (void *)addr, MLX5_WQE64_INL_DATA);
+	addr += MLX5_WQE64_INL_DATA;
+	length -= MLX5_WQE64_INL_DATA;
+
+	while (length) {
+		volatile struct mlx5_wqe64_inl *wqe_next =
+			(volatile struct mlx5_wqe64_inl *)
+			&(*txq->wqes)[(txq->wqe_ci + wqes_cnt) &
+				      (txq->wqe_cnt - 1)];
+		uint32_t copy_bytes = (length > sizeof(*wqe)) ?
+				      sizeof(*wqe) :
+				      length;
+
+		memcpy((void *)(uintptr_t)wqe_next->wqe.data, (void *)addr,
+		       copy_bytes);
+		addr += copy_bytes;
+		length -= copy_bytes;
+		wqes_cnt++;
+	}
+
+	wqe->ctrl.data[0] = htonl((txq->wqe_ci << 8) | MLX5_OPCODE_SEND);
+	wqe->ctrl.data[1] = htonl((txq->qp_num << 8) | (size & 0x3f));
+	if (unlikely(--txq->elts_comp == 0)) {
+		wqe->ctrl.data[2] = htonl(8);
+		txq->elts_comp = txq->elts_comp_cd_init;
+	} else
+		wqe->ctrl.data[2] = htonl(0);
+	wqe->ctrl.data[3] = 0;
+
+	/* Increase the consumer index. */
+	txq->wqe_ci += wqes_cnt;
+}
+
+static inline void
+mlx5_wqe_write_inline_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
+		      uintptr_t addr, uint32_t length, uint16_t *vlan_tci)
+{
+	uint32_t size;
+	uint32_t wqes_cnt = 1;
+	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+	volatile struct mlx5_wqe64_inl *inl_wqe =
+		(volatile struct mlx5_wqe64_inl *)wqe;
+
+	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
+	/*
+	 * Copy 12 bytes of source & destination MAC address.
+	 * Copy 4 bytes of vlan.
+	 * Copy 2 bytes of ether type.
+	 */
+	memcpy((void *)(uintptr_t)wqe->eseg.inline_hdr_start, (void *)(uintptr_t)addr, 12);
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 12), &vlan, sizeof(vlan));
+	memcpy((void *)((uintptr_t)wqe->eseg.inline_hdr_start + 16), (void *)((uintptr_t)addr + 12), 2);
+
+	addr += MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
+	length -= MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
 
 	size = (sizeof(wqe->ctrl.ctrl) +
 		sizeof(wqe->eseg) +
@@ -631,13 +763,22 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		}
 #if MLX5_PMD_MAX_INLINE > 0
 		if (length <= MLX5_PMD_MAX_INLINE)
-			mlx5_wqe_write_inline(txq, wqe, addr, length);
+			if (buf->ol_flags & PKT_TX_VLAN_PKT)
+				mlx5_wqe_write_inline_vlan(txq, wqe,
+							   addr, length,
+							   &buf->vlan_tci);
+			else
+				mlx5_wqe_write_inline(txq, wqe, addr, length);
 		else
 #endif
 		{
 			/* Retrieve Memory Region key for this memory pool. */
 			lkey = txq_mp2mr(txq, txq_mb2mp(buf));
-			mlx5_wqe_write(txq, wqe, addr, length, lkey);
+			if (buf->ol_flags & PKT_TX_VLAN_PKT)
+				mlx5_wqe_write_vlan(txq, wqe, addr, length,
+						    lkey, &buf->vlan_tci);
+			else
+				mlx5_wqe_write(txq, wqe, addr, length, lkey);
 		}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 			sent_size += length;
@@ -665,17 +806,18 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 /**
  * Translate RX completion flags to packet type.
  *
- * @param flags
- *   RX completion flags returned by poll_length_flags().
+ * @param  cqe
+ *   The Pointer to the CQE
  *
  * @return
  *   Packet type for struct rte_mbuf.
  */
 static inline uint32_t
-rxq_cq_to_pkt_type(uint32_t flags)
+rxq_cq_to_pkt_type(volatile struct mlx5_cqe64 *cqe)
 {
 	uint32_t pkt_type;
 
+#if 0 /* Currently IBV_EXP_CQ_RX_TUNNEL_PACKET is not used. */
 	if (flags & IBV_EXP_CQ_RX_TUNNEL_PACKET)
 		pkt_type =
 			TRANSPOSE(flags,
@@ -691,24 +833,70 @@ rxq_cq_to_pkt_type(uint32_t flags)
 				  IBV_EXP_CQ_RX_IPV6_PACKET,
 				  RTE_PTYPE_INNER_L3_IPV6);
 	else
+#endif
 		pkt_type =
-			TRANSPOSE(flags,
-				  IBV_EXP_CQ_RX_IPV4_PACKET,
-				  RTE_PTYPE_L3_IPV4) |
-			TRANSPOSE(flags,
-				  IBV_EXP_CQ_RX_IPV6_PACKET,
-				  RTE_PTYPE_L3_IPV6);
+			TRANSPOSE(cqe->l4_hdr_type_etc,
+				  MLX5_CQE_L3_HDR_TYPE_IPV6,
+				  RTE_PTYPE_L3_IPV6) |
+			TRANSPOSE(cqe->l4_hdr_type_etc,
+				  MLX5_CQE_L3_HDR_TYPE_IPV4,
+				  RTE_PTYPE_L3_IPV4);
 	return pkt_type;
 }
 
+/**
+ * Translate RX completion flags to offload flags.
+ *
+ * @param  cqe
+ *   The Pointer to the CQE
+ *
+ * @return
+ *   Offload flags (ol_flags) for struct rte_mbuf.
+ */
+static inline uint32_t
+rxq_cq_to_ol_flags(volatile struct mlx5_cqe64 *cqe)
+{
+	uint32_t ol_flags = 0;
+	uint8_t l3_hdr = (cqe->l4_hdr_type_etc) & MLX5_CQE_L3_HDR_TYPE_MASK;
+	uint8_t l4_hdr = (cqe->l4_hdr_type_etc) & MLX5_CQE_L4_HDR_TYPE_MASK;
+
+	if ((l3_hdr == MLX5_CQE_L3_HDR_TYPE_IPV4) ||
+	    (l3_hdr == MLX5_CQE_L3_HDR_TYPE_IPV6))
+		ol_flags |=
+			(!(cqe->hds_ip_ext & MLX5_CQE_L3_OK) *
+			 PKT_RX_IP_CKSUM_BAD);
+
+	if ((l4_hdr == MLX5_CQE_L4_HDR_TYPE_TCP) ||
+	    (l4_hdr == MLX5_CQE_L4_HDR_TYPE_TCP_EMP_ACK) ||
+	    (l4_hdr == MLX5_CQE_L4_HDR_TYPE_TCP_ACK) ||
+	    (l4_hdr == MLX5_CQE_L4_HDR_TYPE_UDP))
+		ol_flags |=
+			(!(cqe->hds_ip_ext & MLX5_CQE_L4_OK) *
+			 PKT_RX_L4_CKSUM_BAD);
+#if 0 /* Currently IBV_EXP_CQ_RX_TUNNEL_PACKET is not used. */
+	/*
+	 * PKT_RX_IP_CKSUM_BAD and PKT_RX_L4_CKSUM_BAD are used in place
+	 * of PKT_RX_EIP_CKSUM_BAD because the latter is not functional
+	 * (its value is 0).
+	 */
+	if ((flags & IBV_EXP_CQ_RX_TUNNEL_PACKET) && (rxq->csum_l2tun))
+		ol_flags |=
+			TRANSPOSE(~flags,
+				  IBV_EXP_CQ_RX_OUTER_IP_CSUM_OK,
+				  PKT_RX_IP_CKSUM_BAD) |
+			TRANSPOSE(~flags,
+				  IBV_EXP_CQ_RX_OUTER_TCP_UDP_CSUM_OK,
+				  PKT_RX_L4_CKSUM_BAD);
+#endif
+	return ol_flags;
+}
+
 static inline int
-mlx5_rx_poll_len(struct frxq *rxq)
+mlx5_rx_poll_len(struct frxq *rxq, volatile union mlx5_rx_cqe *cqe)
 {
 	int len = 0;
 	const unsigned int elts_n = 2 * rxq->elts_n;
 	const unsigned int cqe_n = elts_n - 1;
-	volatile union mlx5_rx_cqe *cqe =
-		(volatile union mlx5_rx_cqe *)&(*rxq->cqes)[rxq->cq_ci & cqe_n];
 
 	/* Process the compressed data present in the CQE and its mini arrays. */
 	if (likely(cqe->zip.comp_flag == 1)) {
@@ -818,10 +1006,12 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		struct rte_mbuf *pkt;
 		unsigned int len;
 		volatile struct mlx5_wqe_data_seg *wqe = &(*rxq->wqes)[idx & wqe_cnt];
+		volatile union mlx5_rx_cqe *cqe =
+			(volatile union mlx5_rx_cqe *)
+			&(*rxq->cqes)[rxq->cq_ci & wqe_cnt];
 
 		pkt = (*rxq->elts)[idx];
-		rte_prefetch0((void *)(uintptr_t)
-			      &(*rxq->cqes)[rxq->cq_ci & wqe_cnt]);
+		rte_prefetch0(cqe);
 		rep = __rte_mbuf_raw_alloc(rxq->mp);
 		if (unlikely(rep == NULL)) {
 			wqe->addr = htonll((uintptr_t)pkt->buf_addr +
@@ -833,7 +1023,7 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		NB_SEGS(rep) = 1;
 		PORT(rep) = rxq->port_id;
 		NEXT(rep) = NULL;
-		len = mlx5_rx_poll_len(rxq);
+		len = mlx5_rx_poll_len(rxq, cqe);
 		if (unlikely(len == 0)) {
 			if (rep)
 				__rte_mbuf_raw_free(rep);
@@ -848,7 +1038,14 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		/* Update pkt information. */
 		PKT_LEN(pkt) = len;
 		DATA_LEN(pkt) = len;
-
+		if (rxq->csum | rxq->vlan_strip) {
+			pkt->packet_type = rxq_cq_to_pkt_type(&cqe->cqe64);
+			pkt->ol_flags = rxq_cq_to_ol_flags(&cqe->cqe64);
+			if (cqe->cqe64.l4_hdr_type_etc & MLX5_CQE_VLAN_STRIPPED) {
+				pkt->ol_flags |= PKT_RX_VLAN_PKT;
+				pkt->vlan_tci = ntohs(cqe->cqe64.vlan_info);
+			}
+		}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 		/* Increment bytes counter. */
 		rxq->stats.ibytes += len;

drivers/net/mlx5/mlx5.c
@@ -196,6 +196,8 @@ static const struct eth_dev_ops mlx5_dev_ops = {
 	.reta_query = mlx5_dev_rss_reta_query,
 	.rss_hash_update = mlx5_rss_hash_update,
 	.rss_hash_conf_get = mlx5_rss_hash_conf_get,
+	.vlan_strip_queue_set = mlx5_vlan_strip_queue_set,
+	.vlan_offload_set = mlx5_vlan_offload_set,
 };
 
 static struct {
@@ -344,6 +346,7 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		exp_device_attr.comp_mask =
 			IBV_EXP_DEVICE_ATTR_EXP_CAP_FLAGS |
 			IBV_EXP_DEVICE_ATTR_RX_HASH |
+			IBV_EXP_DEVICE_ATTR_VLAN_OFFLOADS |
 			0;
 #endif /* HAVE_EXP_QUERY_DEVICE */
 
@@ -422,6 +425,11 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 			priv->ind_table_max_size = RSS_INDIRECTION_TABLE_SIZE;
 		DEBUG("maximum RX indirection table size is %u",
 		      priv->ind_table_max_size);
+
+		priv->hw_vlan_strip = !!(exp_device_attr.wq_vlan_offloads_cap &
+					 IBV_EXP_RECEIVE_WQ_CVLAN_STRIP);
+		DEBUG("VLAN stripping is %ssupported",
+		      (priv->hw_vlan_strip ? "" : "not "));
 
 #else /* HAVE_EXP_QUERY_DEVICE */
 		priv->ind_table_max_size = RSS_INDIRECTION_TABLE_SIZE;

drivers/net/mlx5/mlx5.h
@@ -230,6 +230,8 @@ void mlx5_stats_reset(struct rte_eth_dev *);
 /* mlx5_vlan.c */
 
 int mlx5_vlan_filter_set(struct rte_eth_dev *, uint16_t, int);
+void mlx5_vlan_offload_set(struct rte_eth_dev *, int);
+void mlx5_vlan_strip_queue_set(struct rte_eth_dev *, uint16_t, int);
 
 /* mlx5_trigger.c */
 

drivers/net/mlx5/mlx5_hw.h
@@ -60,6 +60,8 @@
 #define MLX5_CQE_INVALIDATE(own) \
 	((MLX5_CQE_INVALID << 4) | (own & MLX5_CQE_OWNER_MASK))
 
+#define MLX5_CQE_VLAN_STRIPPED	0x1
+
 struct mlx5_cqe_comp {
 	uint8_t comp_flag;
 	uint8_t scqe_idx;

drivers/net/mlx5/mlx5_rxq.c
@@ -982,6 +982,12 @@ rxq_setup(struct rte_eth_dev *dev, struct rxq *rxq, uint16_t desc,
 	      priv->device_attr.max_qp_wr);
 	DEBUG("priv->device_attr.max_sge is %d",
 	      priv->device_attr.max_sge);
+	/* Toggle RX checksum offload if hardware supports it. */
+	if (priv->hw_csum)
+		tmpl.frxq.csum = !!dev->data->dev_conf.rxmode.hw_ip_checksum;
+	/* Toggle VLAN stripping if hardware supports it. */
+	if (priv->hw_vlan_strip)
+		tmpl.frxq.vlan_strip = dev->data->dev_conf.rxmode.hw_vlan_strip;
 	attr.wq = (struct ibv_exp_wq_init_attr){
 		.wq_context = NULL, /* Could be useful in the future. */
 		.wq_type = IBV_EXP_WQT_RQ,
@@ -995,8 +1001,12 @@ rxq_setup(struct rte_eth_dev *dev, struct rxq *rxq, uint16_t desc,
 		.cq = tmpl.cq,
 		.comp_mask =
 			IBV_EXP_CREATE_WQ_RES_DOMAIN |
+			IBV_EXP_CREATE_WQ_VLAN_OFFLOADS |
 			0,
 		.res_domain = tmpl.rd,
+		.vlan_offloads = (tmpl.frxq.vlan_strip ?
+				  IBV_EXP_RECEIVE_WQ_CVLAN_STRIP :
+				  0),
 	};
 
 #ifdef HAVE_EXP_CREATE_WQ_FLAG_RX_END_PADDING

drivers/net/mlx5/mlx5_rxtx.h
@@ -97,6 +97,8 @@ struct frxq {
 	uint16_t cq_ci;
 	uint16_t elts_n;
 	uint16_t port_id;
+	unsigned int csum:1; /* Enable checksum offloading. */
+	unsigned int vlan_strip:1; /* Enable VLAN stripping. */
 	volatile struct mlx5_wqe_data_seg (*wqes)[];
 	volatile struct mlx5_cqe64 (*cqes)[];
 	volatile uint32_t *rq_db;

drivers/net/mlx5/mlx5_vlan.c
@@ -129,3 +129,104 @@ mlx5_vlan_filter_set(struct rte_eth_dev *dev, uint16_t vlan_id, int on)
 	assert(ret >= 0);
 	return -ret;
 }
+
+/**
+ * Set/reset VLAN stripping for a specific queue.
+ *
+ * @param priv
+ *   Pointer to private structure.
+ * @param idx
+ *   RX queue index.
+ * @param on
+ *   Enable/disable VLAN stripping.
+ */
+static void
+priv_vlan_strip_queue_set(struct priv *priv, uint16_t idx, int on)
+{
+	struct frxq *frxq = (*priv->rxqs)[idx];
+	struct rxq *rxq = container_of(frxq, struct rxq, frxq);
+	struct ibv_exp_wq_attr mod;
+	uint16_t vlan_offloads =
+		(on ? IBV_EXP_RECEIVE_WQ_CVLAN_STRIP : 0) |
+		0;
+	int err;
+
+	DEBUG("set VLAN offloads 0x%x for port %d queue %d",
+	      vlan_offloads, rxq->port_id, idx);
+	mod = (struct ibv_exp_wq_attr){
+		.attr_mask = IBV_EXP_WQ_ATTR_VLAN_OFFLOADS,
+		.vlan_offloads = vlan_offloads,
+	};
+
+	err = ibv_exp_modify_wq(rxq->wq, &mod);
+	if (err) {
+		ERROR("%p: failed to modified stripping mode: %s",
+		      (void *)priv, strerror(err));
+		return;
+	}
+
+	/* Update related bits in RX queue. */
+	frxq->vlan_strip = !!on;
+}
+
+/**
+ * Callback to set/reset VLAN stripping for a specific queue.
+ *
+ * @param dev
+ *   Pointer to Ethernet device structure.
+ * @param queue
+ *   RX queue index.
+ * @param on
+ *   Enable/disable VLAN stripping.
+ */
+void
+mlx5_vlan_strip_queue_set(struct rte_eth_dev *dev, uint16_t queue, int on)
+{
+	struct priv *priv = dev->data->dev_private;
+
+	/* Validate hw support */
+	if (!priv->hw_vlan_strip) {
+		ERROR("VLAN stripping is not supported");
+		return;
+	}
+
+	/* Validate queue number */
+	if (queue >= priv->rxqs_n) {
+		ERROR("VLAN stripping, invalid queue number %d", queue);
+		return;
+	}
+
+	priv_lock(priv);
+	priv_vlan_strip_queue_set(priv, queue, on);
+	priv_unlock(priv);
+}
+
+/**
+ * Callback to set/reset VLAN offloads for a port.
+ *
+ * @param dev
+ *   Pointer to Ethernet device structure.
+ * @param mask
+ *   VLAN offload bit mask.
+ */
+void
+mlx5_vlan_offload_set(struct rte_eth_dev *dev, int mask)
+{
+	struct priv *priv = dev->data->dev_private;
+	unsigned int i;
+
+	if (mask & ETH_VLAN_STRIP_MASK) {
+		int hw_vlan_strip = dev->data->dev_conf.rxmode.hw_vlan_strip;
+
+		if (!priv->hw_vlan_strip) {
+			ERROR("VLAN stripping is not supported");
+			return;
+		}
+
+		/* Run on every RX queue and set/reset VLAN stripping. */
+		priv_lock(priv);
+		for (i = 0; (i != priv->rxqs_n); i++)
+			priv_vlan_strip_queue_set(priv, i, hw_vlan_strip);
+		priv_unlock(priv);
+	}
+}

commit 45de5092554037317d741f4d63f7f2bd4aa1d9f1

mlx5: fix vlan passing through pointer when is not modified
Passing a pointer of something unchanged in the function may cause an
optimisation issue.

Fixes: d85709205b91 ("mlx5: add missing features")

Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5_rxtx.c
@@ -406,9 +406,9 @@ mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 static inline void
 mlx5_wqe_write_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 		    uintptr_t addr, uint32_t length, uint32_t lkey,
-		    uint16_t *vlan_tci)
+		    uint16_t vlan_tci)
 {
-	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+	uint32_t vlan = htonl(0x81000000 | vlan_tci);
 
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
 	/*
@@ -475,9 +475,9 @@ mlx5_wqe_write(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 static inline void
 mlx5_wqe_write_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 		    uintptr_t addr, uint32_t length, uint32_t lkey,
-		    uint16_t *vlan_tci)
+		    uint16_t vlan_tci)
 {
-	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+	uint32_t vlan = htonl(0x81000000 | vlan_tci);
 
 	wqe->eseg.inline_hdr_sz = htons(MLX5_ETH_VLAN_INLINE_HEADER_SIZE);
 	/*
@@ -574,11 +574,11 @@ mlx5_wqe_write_inline(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
 
 static inline void
 mlx5_wqe_write_inline_vlan(struct ftxq *txq, volatile struct mlx5_wqe64 *wqe,
-			   uintptr_t addr, uint32_t length, uint16_t *vlan_tci)
+			   uintptr_t addr, uint32_t length, uint16_t vlan_tci)
 {
 	uint32_t size;
 	uint32_t wqes_cnt = 1;
-	uint32_t vlan = htonl(0x81000000 | *vlan_tci);
+	uint32_t vlan = htonl(0x81000000 | vlan_tci);
 	volatile struct mlx5_wqe64_inl *inl_wqe =
 		(volatile struct mlx5_wqe64_inl *)wqe;
 
@@ -784,7 +784,7 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 			if (buf->ol_flags & PKT_TX_VLAN_PKT)
 				mlx5_wqe_write_inline_vlan(txq, wqe,
 							   addr, length,
-							   &buf->vlan_tci);
+							   buf->vlan_tci);
 			else
 				mlx5_wqe_write_inline(txq, wqe, addr, length);
 		else
@@ -794,7 +794,7 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 			lkey = txq_mp2mr(txq, txq_mb2mp(buf));
 			if (buf->ol_flags & PKT_TX_VLAN_PKT)
 				mlx5_wqe_write_vlan(txq, wqe, addr, length,
-						    lkey, &buf->vlan_tci);
+						    lkey, buf->vlan_tci);
 			else
 				mlx5_wqe_write(txq, wqe, addr, length, lkey);
 		}