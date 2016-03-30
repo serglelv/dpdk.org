serglelv/dpdk.org

commit 24fd770732b10c86eb37f21e9685bd7dea176ff8

mlx5: handle CQE compression
Mini CQEs are used by the NIC when the PCI bandwidth is limited, in this case,
the first CQE64 received contains almost everything and the number following
CQE8, following CQEs are empty and used by the software for decompression.

From this:

      0           1          2           6         7         8
  +-------+  +---------+ +-------+   +-------+ +-------+ +-------+
  | CQE64 |  |  CQE64  | | CQE64 |   | CQE64 | | CQE64 | | CQE64 |
  |-------|  |---------| |-------|   |-------| |-------| |-------|
  | ..... |  | cqe8[0] | |       | . |       | |       | | ..... |
  | ..... |  | cqe8[1] | |       | . |       | |       | | ..... |
  | ..... |  | ....... | |       | . |       | |       | | ..... |
  | ..... |  | cqe8[7] | |       |   |       | |       | | ..... |
  +-------+  +---------+ +-------+   +-------+ +-------+ +-------+

We get this after decompression:

      0          1     ...     8
  +-------+  +-------+     +-------+
  | CQE64 |  | CQE64 |     | CQE64 |
  |-------|  |-------|     |-------|
  | ..... |  | ..... |  .  | ..... |
  | ..... |  | ..... |  .  | ..... |
  | ..... |  | ..... |  .  | ..... |
  | ..... |  | ..... |     | ..... |
  +-------+  +-------+     +-------+

This decompression cost a lot in resources knowing that the software only
needs the first CQE64 and the arrays of CQE8.

This patch provide avoid to recreate each cqe, it will ends by consuming more
CQEs than the uncompressed version because until all the compressed data is
not consumed, the CQEs associated will be kept.

Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5_hw.h
@@ -53,8 +53,55 @@
 /* Get CQE op code. */
 #define MLX5_CQE_OPCODE(op_own) ((op_own) >> 4)
 
+/* Get CQE Solicited Event. */
+#define MLX5_CQE_SE(op_own) (((op_own) >> 1) & 1)
+
 /* Invalidate a CQE. */
 #define MLX5_CQE_INVALIDATE(own) \
 	((MLX5_CQE_INVALID << 4) | (own & MLX5_CQE_OWNER_MASK))
+
+struct mlx5_cqe_comp {
+	uint8_t comp_flag;
+	uint8_t scqe_idx;
+	uint16_t wqe_id;
+	uint16_t cq_ci_narray;
+	uint16_t cq_ci_carray;
+	uint8_t	rsvd4[4];
+	uint32_t rx_hash_res;
+	uint8_t rx_hash_type;
+	uint8_t ml_path;
+	uint16_t cqe_ci;
+	uint16_t checksum;
+	uint16_t slid;
+	uint32_t flags_rqpn;
+	uint8_t hds_ip_ext;
+	uint8_t l4_hdr_type_etc;
+	__be16 vlan_info;
+	uint32_t srqn_uidx;
+	uint32_t imm_inval_pkey;
+	uint16_t rsvd40;
+	uint16_t cq_ci;
+	uint32_t cqe_cnt;
+	__be64	 timestamp;
+	union {
+		uint32_t sop_drop_qpn;
+		struct {
+			uint8_t	sop;
+			uint8_t qpn[3];
+		} sop_qpn;
+	} sop;
+	/*
+	 * In Striding RQ (Multi-Packet RQ) wqe_counter provides
+	 * the WQE stride index (to calc pointer to start of the message)
+	 */
+	uint16_t wqe_cnt;
+	uint8_t signature;
+	uint8_t op_own;
+} __attribute__((aligned(64)));
+
+union mlx5_rx_cqe {
+	struct mlx5_cqe64 cqe64;
+	struct mlx5_cqe_comp zip;
+};
 
 #endif /* RTE_PMD_MLX5_HW_H_ */

drivers/net/mlx5/mlx5_rxtx.c
@@ -107,6 +107,50 @@ check_cqe64(volatile struct mlx5_cqe64 *cqe,
 }
 
 /**
+ * Manage TX compressed completions.
+ *
+ * @param txq
+ *   Pointer to TX queue structure.
+ * @param  cqe
+ *   Poniter to the compressed CQE.
+ * @param cq_ci
+ *   Pointer to the current consumer index.
+ *
+ * @retrun
+ *   The wqe_ci corresponding to the last packet sent.
+ */
+static inline uint16_t
+txq_complete_compressed(struct ftxq *txq, volatile struct mlx5_cqe64 *cqe,
+			uint16_t *cq_ci)
+{
+	volatile struct mlx5_mini_cqe8 (*mc)[8];
+	uint32_t cqe_cnt = ntohl(cqe->byte_cnt);
+	const unsigned int cqe_n = MLX5_TX_CQ_SIZE - 1;
+	uint16_t mc_pos;
+	uint16_t wqe_ci;
+
+
+	/* The position of the compressed array is after the title CQE, here
+	 * the title CQE is the variable cqe, the next position are in
+	 * multiple of 8 (8 bytes per mini CQE in a single 64 byte CQE)
+	 * if they exists. */
+	mc_pos = ((*cq_ci) + 1 + (cqe_cnt & 0xf8)) & cqe_n;
+	/* First get the last wqe_ci from the mini cqe. */
+	mc = (volatile struct mlx5_mini_cqe8 (*)[8])
+		(uintptr_t)&(*txq->cqes)[mc_pos];
+	wqe_ci = ntohs((*mc)[(cqe_cnt - 1) & 7].s_wqe_info.wqe_counter);
+	/* Reset the owner bit in all CQE. */
+	while(cqe_cnt) {
+		cqe = &(*txq->cqes)[(*cq_ci) & cqe_n];
+		cqe->op_own = MLX5_CQE_INVALIDATE(0);
+		++(*cq_ci);
+		--cqe_cnt;
+	}
+
+	return wqe_ci;
+}
+
+/**
  * Manage TX completions.
  *
  * When sending a burst, mlx5_tx_burst() posts several WRs.
@@ -120,32 +164,33 @@ check_cqe64(volatile struct mlx5_cqe64 *cqe,
 static void
 txq_complete(struct ftxq *txq)
 {
-	unsigned int elts_tail = txq->elts_tail;
-	unsigned int elts_free = txq->elts_tail;
 	const unsigned int elts_n = txq->elts_n;
+	const unsigned int cqe_n = MLX5_TX_CQ_SIZE - 1;
+	uint16_t elts_free = txq->elts_tail;
+	uint16_t elts_tail;
+	uint16_t cq_ci = txq->cq_ci;
 	unsigned int wqe_ci = (unsigned int)-1;
 	int ret = 0;
 
 	while (ret == 0) {
-		unsigned int idx = txq->cq_ci & (elts_n - 1);
+		unsigned int idx = cq_ci & cqe_n;
 		volatile struct mlx5_cqe64 *cqe = &(*txq->cqes)[idx];
-		uint8_t op_own = cqe->op_own;
 
-		ret = check_cqe64(cqe, elts_n, txq->cq_ci);
-		if (ret == 0) {
-			if (likely(MLX5_CQE_OPCODE(op_own) == MLX5_CQE_REQ))
-				wqe_ci = ntohs(cqe->wqe_counter);
-			++txq->cq_ci;
-		} else
+		ret = check_cqe64(cqe, MLX5_TX_CQ_SIZE, cq_ci);
+		if (ret == 1)
 			break;
+
+		if (MLX5_CQE_FORMAT(cqe->op_own) == MLX5_COMPRESSED)
+			wqe_ci = txq_complete_compressed(txq, cqe, &cq_ci);
+		else if ((MLX5_CQE_OPCODE(cqe->op_own) == MLX5_CQE_REQ)) {
+			wqe_ci = ntohs(cqe->wqe_counter);
+			++cq_ci;
+		}
 	}
 	if (unlikely(wqe_ci == (unsigned int)-1))
 		return;
-
+	/* Free buffers. */
 	elts_tail = wqe_ci & (elts_n - 1);
-	if (elts_tail >= elts_n)
-		elts_tail -= elts_n;
-
 	while (elts_free != elts_tail) {
 		struct rte_mbuf *elt = (*txq->elts)[elts_free];
 		unsigned int elts_free_next =
@@ -163,6 +208,7 @@ txq_complete(struct ftxq *txq)
 		} while (elt != NULL);
 		elts_free = elts_free_next;
 	}
+	txq->cq_ci = cq_ci;
 	txq->elts_tail = elts_tail;
 	/* Update the consumer index. */
 	rte_wmb();
@@ -454,6 +500,91 @@ rxq_cq_to_pkt_type(uint32_t flags)
 	return pkt_type;
 }
 
+static inline int
+mlx5_rx_poll_len(struct frxq *rxq)
+{
+	int len = 0;
+	const unsigned int elts_n = 2 * rxq->elts_n;
+	const unsigned int cqe_n = elts_n - 1;
+	volatile union mlx5_rx_cqe *cqe =
+		(volatile union mlx5_rx_cqe *)&(*rxq->cqes)[rxq->cq_ci & cqe_n];
+
+	/* Process the compressed data present in the CQE and its mini arrays. */
+	if (likely(cqe->zip.comp_flag == 1)) {
+		volatile struct mlx5_mini_cqe8 (*mc)[8] =
+			(volatile struct mlx5_mini_cqe8 (*)[8])
+			(uintptr_t)&(*rxq->cqes)[cqe->zip.cq_ci_carray & cqe_n];
+
+		len = ntohl((*mc)[cqe->zip.scqe_idx & 7].byte_cnt);
+		if ((++cqe->zip.scqe_idx & 7) == 0) {
+			/* Increase the consumer index to jump the number of
+			 * CQE consumed.  The hardware leave holes in CQ ring
+			 * for the software. */
+			cqe->zip.cq_ci_carray = cqe->zip.cq_ci_narray;
+			cqe->zip.cq_ci_narray += 8;
+		}
+
+		if (unlikely(cqe->zip.scqe_idx == cqe->zip.cqe_cnt)) {
+			uint16_t idx = rxq->cq_ci;
+			uint16_t end = cqe->zip.cq_ci;
+			unsigned int log = log2above(elts_n);
+
+			while (idx != end) {
+				(*rxq->cqes)[idx & cqe_n].op_own =
+					MLX5_CQE_INVALIDATE((idx >> log) & 1);
+				++idx;
+			}
+			rxq->cq_ci = cqe->zip.cq_ci;
+			cqe->zip.comp_flag = 0;
+		}
+	/* No compressed data, get the next CQE and verify if it compressed. */
+	} else {
+		int ret;
+		int8_t op_own;
+
+		ret = check_cqe64(&cqe->cqe64, elts_n, rxq->cq_ci);
+		if (unlikely(ret == 1))
+			return 0;
+		++rxq->cq_ci;
+		op_own = cqe->cqe64.op_own;
+
+		if (MLX5_CQE_FORMAT(op_own) == MLX5_COMPRESSED) {
+			volatile struct mlx5_mini_cqe8 (*mc)[8] =
+				(volatile struct mlx5_mini_cqe8 (*)[8])
+				(uintptr_t)&(*rxq->cqes)[rxq->cq_ci & cqe_n];
+
+			/* Update big endian fields to little endian ones. */
+			cqe->zip.cqe_cnt = ntohl(cqe->cqe64.byte_cnt);
+			cqe->zip.wqe_cnt = ntohs(cqe->cqe64.wqe_counter);
+
+			cqe->zip.comp_flag = 1;
+			/* Current mini array position is the one returned by
+			 * check_cqe64() which increased by itself the
+			 * consumer index.
+			 *
+			 * The next position if available of the mini array
+			 * will be in 7 position from the current array
+			 * position.  It is a special case, after this one,
+			 * the next mini array will 8 CQEs after.
+			 */
+			cqe->zip.cq_ci_carray = rxq->cq_ci & cqe_n;
+			cqe->zip.cq_ci_narray = cqe->zip.cq_ci_carray + 7;
+
+			/* Compute the next non compressed CQE. */
+			--rxq->cq_ci;
+			cqe->zip.cq_ci = rxq->cq_ci + cqe->zip.cqe_cnt;
+
+			/* Get the packet size and return it. */
+			len = ntohl((*mc)[0].byte_cnt);
+			cqe->zip.scqe_idx = 1;
+		} else {
+			len = ntohl(cqe->cqe64.byte_cnt);
+		}
+	}
+
+	return len;
+}
+
 /**
  * DPDK callback for RX.
  *
@@ -485,12 +616,11 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		struct rte_mbuf *rep;
 		struct rte_mbuf *pkt;
 		unsigned int len;
-		unsigned int ret;
 		volatile struct mlx5_wqe_data_seg *wqe = &(*rxq->wqes)[idx & wqe_cnt];
-		volatile struct mlx5_cqe64 *cqe = &(*rxq->cqes)[rxq->cq_ci & wqe_cnt];
 
 		pkt = (*rxq->elts)[idx];
-		rte_prefetch0(cqe);
+		rte_prefetch0((void *)(uintptr_t)
+			      &(*rxq->cqes)[rxq->cq_ci & wqe_cnt]);
 		rep = __rte_mbuf_raw_alloc(rxq->mp);
 		if (unlikely(rep == NULL)) {
 			wqe->addr = htonll((uintptr_t)pkt->buf_addr +
@@ -502,14 +632,12 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		NB_SEGS(rep) = 1;
 		PORT(rep) = rxq->port_id;
 		NEXT(rep) = NULL;
-		ret = check_cqe64(cqe, rxq->elts_n, rxq->cq_ci);
-		if (unlikely(ret != 0)) {
+		len = mlx5_rx_poll_len(rxq);
+		if (unlikely(len == 0)) {
 			if (rep)
 				__rte_mbuf_raw_free(rep);
 			break;
 		}
-		++rxq->cq_ci;
-		len = ntohl(cqe->byte_cnt);
 		/* Fill NIC descriptor with the new buffer.  The lkey and size
 		 * of the buffers are already known, only the buffer address
 		 * changes. */

drivers/net/mlx5/mlx5_rxtx.h
@@ -241,7 +241,7 @@ struct ftxq {
 	uint16_t wqe_ci;
 	uint16_t bf_offset;
 	uint16_t bf_buf_size;
-	volatile struct mlx5_cqe64 (*cqes)[];
+	volatile struct mlx5_cqe64 (*cqes)[MLX5_TX_CQ_SIZE];
 	volatile struct mlx5_wqe64 (*wqes)[];
 	volatile uint32_t *qp_db;
 	volatile uint32_t *cq_db;

drivers/net/mlx5/mlx5_defs.h
@@ -54,6 +54,9 @@
 /* RSS Indirection table size. */
 #define RSS_INDIRECTION_TABLE_SIZE 512
 
+/* TX Completion Queue size. */
+#define MLX5_TX_CQ_SIZE 16
+
 /*
  * Maximum number of cached Memory Pools (MPs) per TX queue. Each RTE MP
  * from which buffers are to be transmitted will have to be mapped by this

drivers/net/mlx5/mlx5_rxq.c
@@ -970,7 +970,7 @@ rxq_setup(struct rte_eth_dev *dev, struct rxq *rxq, uint16_t desc,
 		.comp_mask = IBV_EXP_CQ_INIT_ATTR_RES_DOMAIN,
 		.res_domain = tmpl.rd,
 	};
-	tmpl.cq = ibv_exp_create_cq(priv->ctx, desc - 1, NULL, NULL, 0,
+	tmpl.cq = ibv_exp_create_cq(priv->ctx, (2 * desc) - 1, NULL, NULL, 0,
 				    &attr.cq);
 	if (tmpl.cq == NULL) {
 		ret = ENOMEM;

drivers/net/mlx5/mlx5_txq.c
@@ -280,7 +280,7 @@ txq_setup(struct rte_eth_dev *dev, struct txq *txq, uint16_t desc,
 		.comp_mask = IBV_EXP_CQ_INIT_ATTR_RES_DOMAIN,
 		.res_domain = tmpl.rd,
 	};
-	tmpl.cq = ibv_exp_create_cq(priv->ctx, desc - 1,
+	tmpl.cq = ibv_exp_create_cq(priv->ctx, MLX5_TX_CQ_SIZE - 1,
 				    NULL, NULL, 0, &attr.cq);
 	if (tmpl.cq == NULL) {
 		ret = ENOMEM;

commit fed9fd32007d5a2f1f66f4726cabf6dea4a3765d

mlx5: fix avoid to store data in the CQE
The NIC continues to prefetch the CQE while the software is modifying it to
store context data for the compression.  As there are no barrier, once it
comes back in the cache, previous written data by the software where erase
causing unexpected issues.

Fixes: 24fd770732b1 ("mlx5: handle CQE compression")

Signed-off-by: Nélio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5_hw.h
@@ -61,48 +61,4 @@
 
 #define MLX5_CQE_VLAN_STRIPPED	0x1
 
-struct mlx5_cqe_comp {
-	uint8_t rsvd0;
-	uint8_t scqe_idx;
-	uint16_t wqe_id;
-	uint16_t cq_ci_narray;
-	uint16_t cq_ci_carray;
-	uint8_t	rsvd4[4];
-	uint32_t rx_hash_res;
-	uint8_t rx_hash_type;
-	uint8_t ml_path;
-	uint16_t cqe_ci;
-	uint16_t checksum;
-	uint16_t slid;
-	uint32_t flags_rqpn;
-	uint8_t hds_ip_ext;
-	uint8_t l4_hdr_type_etc;
-	__be16 vlan_info;
-	uint32_t srqn_uidx;
-	uint32_t imm_inval_pkey;
-	uint16_t rsvd40;
-	uint16_t cq_ci;
-	uint32_t cqe_cnt;
-	__be64	 timestamp;
-	union {
-		uint32_t sop_drop_qpn;
-		struct {
-			uint8_t	sop;
-			uint8_t qpn[3];
-		} sop_qpn;
-	} sop;
-	/*
-	 * In Striding RQ (Multi-Packet RQ) wqe_counter provides
-	 * the WQE stride index (to calc pointer to start of the message)
-	 */
-	uint16_t wqe_cnt;
-	uint8_t signature;
-	uint8_t op_own;
-} __attribute__((aligned(64)));
-
-union mlx5_rx_cqe {
-	struct mlx5_cqe64 cqe64;
-	struct mlx5_cqe_comp zip;
-};
-
 #endif /* RTE_PMD_MLX5_HW_H_ */

drivers/net/mlx5/mlx5_rxtx.c
@@ -912,50 +912,50 @@ rxq_cq_to_ol_flags(volatile struct mlx5_cqe64 *cqe)
 }
 
 static inline int
-mlx5_rx_poll_len(struct frxq *rxq, volatile union mlx5_rx_cqe *cqe,
+mlx5_rx_poll_len(struct frxq *rxq, volatile struct mlx5_cqe64 *cqe,
 		 uint16_t cqe_cnt)
 {
+	struct rxq_zip *zip = &rxq->zip;
 	uint16_t cqe_n = cqe_cnt + 1;
 	int len = 0;
 
 	/* Process the compressed data present in the CQE and its mini arrays. */
-	if (likely(rxq->compressed)) {
+	if (likely(zip->ai)) {
 		volatile struct mlx5_mini_cqe8 (*mc)[8] =
 			(volatile struct mlx5_mini_cqe8 (*)[8])
-			(uintptr_t)&(*rxq->cqes)[cqe->zip.cq_ci_carray &
-						 cqe_cnt];
+			(uintptr_t)&(*rxq->cqes)[zip->ca & cqe_cnt];
 
-		len = ntohl((*mc)[cqe->zip.scqe_idx & 7].byte_cnt);
-		if ((++cqe->zip.scqe_idx & 7) == 0) {
+		len = ntohl((*mc)[zip->ai & 7].byte_cnt);
+		if ((++zip->ai & 7) == 0) {
 			/* Increase the consumer index to jump the number of
 			 * CQE consumed.  The hardware leave holes in CQ ring
 			 * for the software. */
-			cqe->zip.cq_ci_carray = cqe->zip.cq_ci_narray;
-			cqe->zip.cq_ci_narray += 8;
+			zip->ca = zip->na;
+			zip->na += 8;
 		}
 
-		if (unlikely(cqe->zip.scqe_idx == cqe->zip.cqe_cnt)) {
+		if (unlikely(rxq->zip.ai == rxq->zip.cqe_cnt)) {
 			uint16_t idx = rxq->cq_ci;
-			uint16_t end = cqe->zip.cq_ci;
+			uint16_t end = zip->cq_ci;
 
 			while (idx != end) {
 				(*rxq->cqes)[idx & cqe_cnt].op_own =
 					MLX5_CQE_INVALIDATE;
 				++idx;
 			}
-			rxq->cq_ci = cqe->zip.cq_ci;
-			rxq->compressed = 0;
+			rxq->cq_ci = zip->cq_ci;
+			zip->ai = 0;
 		}
 	/* No compressed data, get the next CQE and verify if it compressed. */
 	} else {
 		int ret;
 		int8_t op_own;
 
-		ret = check_cqe64(&cqe->cqe64, cqe_n, rxq->cq_ci);
+		ret = check_cqe64(cqe, cqe_n, rxq->cq_ci);
 		if (unlikely(ret == 1))
 			return 0;
 		++rxq->cq_ci;
-		op_own = cqe->cqe64.op_own;
+		op_own = cqe->op_own;
 
 		if (MLX5_CQE_FORMAT(op_own) == MLX5_COMPRESSED) {
 			volatile struct mlx5_mini_cqe8 (*mc)[8] =
@@ -964,10 +964,8 @@ mlx5_rx_poll_len(struct frxq *rxq, volatile union mlx5_rx_cqe *cqe,
 							 cqe_cnt];
 
 			/* Update big endian fields to little endian ones. */
-			cqe->zip.cqe_cnt = ntohl(cqe->cqe64.byte_cnt);
-			cqe->zip.wqe_cnt = ntohs(cqe->cqe64.wqe_counter);
+			zip->cqe_cnt = ntohl(cqe->byte_cnt);
 
-			rxq->compressed = 1;
 			/* Current mini array position is the one returned by
 			 * check_cqe64() which increased by itself the
 			 * consumer index.
@@ -977,19 +975,18 @@ mlx5_rx_poll_len(struct frxq *rxq, volatile union mlx5_rx_cqe *cqe,
 			 * position.  It is a special case, after this one,
 			 * the next mini array will 8 CQEs after.
 			 */
-			cqe->zip.cq_ci_carray = rxq->cq_ci & cqe_cnt;
-			cqe->zip.cq_ci_narray = cqe->zip.cq_ci_carray + 7;
+			zip->ca = rxq->cq_ci & cqe_cnt;
+			zip->na = zip->ca + 7;
 
 			/* Compute the next non compressed CQE. */
 			--rxq->cq_ci;
-			cqe->zip.cq_ci = rxq->cq_ci + cqe->zip.cqe_cnt;
+			zip->cq_ci = rxq->cq_ci + zip->cqe_cnt;
 
 			/* Get the packet size and return it. */
 			len = ntohl((*mc)[0].byte_cnt);
-			cqe->zip.scqe_idx = 1;
-		} else {
-			len = ntohl(cqe->cqe64.byte_cnt);
-		}
+			zip->ai = 1;
+		} else
+			len = ntohl(cqe->byte_cnt);
 	}
 
 	return len;
@@ -1027,9 +1024,9 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		struct rte_mbuf *rep;
 		struct rte_mbuf *pkt;
 		unsigned int len;
-		volatile struct mlx5_wqe_data_seg *wqe = &(*rxq->wqes)[idx & wqe_cnt];
-		volatile union mlx5_rx_cqe *cqe =
-			(volatile union mlx5_rx_cqe *)
+		volatile struct mlx5_wqe_data_seg *wqe =
+			&(*rxq->wqes)[idx & wqe_cnt];
+		volatile struct mlx5_cqe64 *cqe =
 			&(*rxq->cqes)[rxq->cq_ci & cqe_cnt];
 
 		pkt = (*rxq->elts)[idx];
@@ -1062,14 +1059,13 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		if (rxq->csum | rxq->vlan_strip | rxq->crc_present) {
 			if (rxq->csum) {
 				pkt->packet_type =
-					rxq_cq_to_pkt_type(&cqe->cqe64);
+					rxq_cq_to_pkt_type(cqe);
 				pkt->ol_flags =
-					rxq_cq_to_ol_flags(&cqe->cqe64);
+					rxq_cq_to_ol_flags(cqe);
 			}
-			if (cqe->cqe64.l4_hdr_type_etc &
-			    MLX5_CQE_VLAN_STRIPPED) {
+			if (cqe->l4_hdr_type_etc & MLX5_CQE_VLAN_STRIPPED) {
 				pkt->ol_flags |= PKT_RX_VLAN_PKT;
-				pkt->vlan_tci = ntohs(cqe->cqe64.vlan_info);
+				pkt->vlan_tci = ntohs(cqe->vlan_info);
 			}
 			if (rxq->crc_present)
 				len -= CRC_SIZE;

drivers/net/mlx5/mlx5_rxtx.h
@@ -90,6 +90,14 @@ struct fdir_queue {
 
 struct priv;
 
+struct rxq_zip {
+	uint16_t ai; /* Array index. */
+	uint16_t ca; /* Current array index. */
+	uint16_t na; /* Next array index. */
+	uint16_t cq_ci; /* The next CQE. */
+	uint32_t cqe_cnt;
+};
+
 /* RX queue descriptor. */
 struct frxq {
 	uint16_t idx;
@@ -98,16 +106,16 @@ struct frxq {
 	uint16_t elts_n;
 	uint16_t cqe_cnt;
 	uint16_t port_id;
-	unsigned int compressed:1; /* Set when a compressed CQE is being processed. */
-	unsigned int csum:1; /* Enable checksum offloading. */
-	unsigned int vlan_strip:1; /* Enable VLAN stripping. */
-	unsigned int crc_present:1; /* CRC must be subtracted. */
+	struct rxq_zip zip; /* Compressed context. */
 	volatile struct mlx5_wqe_data_seg (*wqes)[];
 	volatile struct mlx5_cqe64 (*cqes)[];
 	volatile uint32_t *rq_db;
 	volatile uint32_t *cq_db;
 	struct rte_mbuf *(*elts)[];
 	struct rte_mempool *mp;
+	unsigned int csum:1; /* Enable checksum offloading. */
+	unsigned int vlan_strip:1; /* Enable VLAN stripping. */
+	unsigned int crc_present:1; /* CRC must be subtracted. */
 	struct mlx5_rxq_stats stats;
 } __attribute__((aligned(64)));
 