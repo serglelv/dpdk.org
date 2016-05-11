serglelv/dpdk.org

commit 66aa2a7e8c8f16ac6b8234e5b3531a43dd1bb8e6

mlx5: detect SR-IOV device
If the PF has VF activated or the driver is initialised on a VF, a flag is
setted in priv structure.

Signed-off-by: Nélio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5.c
@@ -258,7 +258,7 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 	int err = 0;
 	struct ibv_context *attr_ctx = NULL;
 	struct ibv_device_attr device_attr;
-	unsigned int vf;
+	unsigned int sriov;
 	int idx;
 	int i;
 
@@ -300,12 +300,14 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		    (pci_dev->addr.devid != pci_addr.devid) ||
 		    (pci_dev->addr.function != pci_addr.function))
 			continue;
-		vf = ((pci_dev->id.device_id ==
+		sriov = ((pci_dev->id.device_id ==
 		       PCI_DEVICE_ID_MELLANOX_CONNECTX4VF) ||
 		      (pci_dev->id.device_id ==
 		       PCI_DEVICE_ID_MELLANOX_CONNECTX4LXVF));
-		INFO("PCI information matches, using device \"%s\" (VF: %s)",
-		     list[i]->name, (vf ? "true" : "false"));
+		INFO("PCI information matches, using device \"%s\""
+		     " (SR-IOV: %s)",
+		     list[i]->name,
+		     sriov ? "true" : "false");
 		attr_ctx = ibv_open_device(list[i]);
 		err = errno;
 		break;
@@ -342,6 +344,7 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		struct ibv_exp_device_attr exp_device_attr;
 #endif /* HAVE_EXP_QUERY_DEVICE */
 		struct ether_addr mac;
+		uint16_t num_vfs = 0;
 
 #ifdef HAVE_EXP_QUERY_DEVICE
 		exp_device_attr.comp_mask =
@@ -441,7 +444,13 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		priv->ind_table_max_size = RSS_INDIRECTION_TABLE_SIZE;
 #endif /* HAVE_EXP_QUERY_DEVICE */
 
-		priv->vf = vf;
+		err = priv_get_num_vfs(priv, &num_vfs);
+		if (err) {
+			ERROR("cannot read sysfs num_vfs entry (errno: %s)",
+			      strerror(errno));
+			goto port_error;
+		}
+		priv->sriov = (num_vfs || sriov);
 		/* Allocate and register default RSS hash keys. */
 		priv->rss_conf = rte_calloc(__func__, hash_rxq_init_n,
 					    sizeof((*priv->rss_conf)[0]), 0);

drivers/net/mlx5/mlx5_ethdev.c
@@ -363,6 +363,31 @@ priv_ifreq(const struct priv *priv, int req, struct ifreq *ifr)
 }
 
 /**
+ * Get number of present VFs on the device
+ *
+ * @param priv
+ *   Pointer to private structure.
+ * @param[out] num_vfs
+ *   Number of active VFs value output buffer.
+ *
+ * @return
+ *   0 on success, -1 on failure and errno is set.
+ */
+int
+priv_get_num_vfs(struct priv *priv, uint16_t *num_vfs)
+{
+	unsigned long ulong_num_vfs;
+	int ret;
+
+	ret = priv_get_sysfs_ulong(priv, "device/sriov_numvfs",
+				   &ulong_num_vfs);
+	if (ret == -1)
+		return -1;
+	*num_vfs = ulong_num_vfs;
+	return 0;
+}
+
+/**
  * Get device MTU.
  *
  * @param priv

drivers/net/mlx5/mlx5.h
@@ -104,7 +104,7 @@ struct priv {
 	unsigned int hw_csum_l2tun:1; /* Same for L2 tunnels. */
 	unsigned int hw_vlan_strip:1; /* VLAN stripping is supported. */
 	unsigned int hw_fcs_strip:1; /* FCS stripping is supported. */
-	unsigned int vf:1; /* This is a VF device. */
+	unsigned int sriov:1; /* This is a VF or PF with VF devices. */
 	unsigned int mpw_en:1; /* Enable Multi-Packet WQE. */
 	unsigned int pending_alarm:1; /* An alarm is pending. */
 	unsigned int txq_inline:1; /* Inline is requested for TX. */
@@ -173,6 +173,7 @@ struct priv *mlx5_get_priv(struct rte_eth_dev *dev);
 int mlx5_is_secondary(void);
 int priv_get_ifname(const struct priv *, char (*)[IF_NAMESIZE]);
 int priv_ifreq(const struct priv *, int req, struct ifreq *);
+int priv_get_num_vfs(struct priv *, uint16_t *);
 int priv_get_mtu(struct priv *, uint16_t *);
 int priv_set_flags(struct priv *, unsigned int, unsigned int);
 int mlx5_dev_configure(struct rte_eth_dev *);

drivers/net/mlx5/mlx5_txq.c
@@ -528,7 +528,7 @@ txq_setup(struct rte_eth_dev *dev, struct txq *txq, uint16_t desc,
 	};
 #if defined(HAVE_EXP_QP_BURST_CREATE_ENABLE_MULTI_PACKET_SEND_WR)
 	/* Multi packet send WR can only be used outside of VF. */
-	if (!priv->vf && priv->mpw_en)
+	if (!priv->sriov && priv->mpw_en)
 		attr.params.family_flags =
 			IBV_EXP_QP_BURST_CREATE_ENABLE_MULTI_PACKET_SEND_WR;
 #endif

commit dcf31f2a4b6eea7d2b85fe9e1e6ab962006c8b04

mlx5: fix SR-IOV check by reading mlx5_num_vfs file too
On redhat operating system sriov_numvfs file is named mlx5_num_vfs, if the
first is not available, we need to check the second one.

If none is present we trust the user and only print a warning.

Fixes: 66aa2a7e8c8f ("mlx5: detect SR-IOV device")

Signed-off-by: Nélio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5.c
@@ -588,9 +588,8 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 
 		err = priv_get_num_vfs(priv, &num_vfs);
 		if (err) {
-			ERROR("cannot read sysfs num_vfs entry (errno: %s)",
+			WARN("cannot read sysfs num_vfs entry (errno: %s)",
 			      strerror(errno));
-			goto port_error;
 		}
 		priv->sriov = (num_vfs || sriov);
 		priv->mps = mps;

drivers/net/mlx5/mlx5_ethdev.c
@@ -379,11 +379,19 @@ priv_get_num_vfs(struct priv *priv, uint16_t *num_vfs)
 	unsigned long ulong_num_vfs;
 	int ret;
 
+	*num_vfs = 0;
 	ret = priv_get_sysfs_ulong(priv, "device/sriov_numvfs",
 				   &ulong_num_vfs);
-	if (ret == -1)
-		return -1;
+	if (ret == -1) {
+		/* Depending on the operating system, the file may not have
+		 * the same name. */
+		ret = priv_get_sysfs_ulong(priv, "device/mlx5_num_vfs",
+				&ulong_num_vfs);
+		if (ret == -1)
+			return -1;
+	}
 	*num_vfs = ulong_num_vfs;
+
 	return 0;
 }
 