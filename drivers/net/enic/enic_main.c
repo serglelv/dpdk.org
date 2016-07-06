DPDK/dpdk

mbuf: add raw allocation function
Many drivers provide their own implementation of rte_mbuf_raw_alloc(),
duplicating the code. Introduce a new public function in rte_mbuf to
allocate a raw mbuf (uninitialized).

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

drivers/net/enic/enic_main.c
@@ -60,17 +60,6 @@
 #include "vnic_nic.h"
 #include "enic_vnic_wq.h"
 
-static inline struct rte_mbuf *
-rte_rxmbuf_alloc(struct rte_mempool *mp)
-{
-	struct rte_mbuf *m;
-
-	m = __rte_mbuf_raw_alloc(mp);
-	__rte_mbuf_sanity_check_raw(m, 0);
-	return m;
-}
-
-
 static inline int enic_is_sriov_vf(struct enic *enic)
 {
 	return enic->pdev->id.device_id == PCI_DEVICE_ID_CISCO_VIC_ENET_VF;
@@ -347,7 +336,7 @@ enic_alloc_rx_queue_mbufs(struct enic *enic, struct vnic_rq *rq)
 		  rq->ring.desc_count);
 
 	for (i = 0; i < rq->ring.desc_count; i++, rqd++) {
-		mb = rte_rxmbuf_alloc(rq->mp);
+		mb = rte_mbuf_raw_alloc(rq->mp);
 		if (mb == NULL) {
 			dev_err(enic, "RX mbuf alloc failed queue_id=%u\n",
 			(unsigned)rq->index);

serglelv/dpdk.org

(cherry picked from commit fbfd99551ca370266f4bfff58ce441cf5cb1203a)
Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>
Conflicts:
	app/test-pmd/flowgen.c
	app/test-pmd/txonly.c
	drivers/net/e1000/em_rxtx.c
	drivers/net/e1000/igb_rxtx.c
	drivers/net/enic/enic_main.c
	drivers/net/enic/enic_rx.c
	drivers/net/ixgbe/ixgbe_rxtx.c
	drivers/net/mlx5/mlx5_rxtx.c
	drivers/net/mpipe/mpipe_tilegx.c
	drivers/net/qede/qede_rxtx.c
	lib/librte_mbuf/rte_mbuf.h

drivers/net/enic/enic_main.c
@@ -80,16 +80,6 @@ static int is_eth_addr_valid(uint8_t *addr)
 	return !is_mcast_addr(addr) && !is_zero_addr(addr);
 }
 
-static inline struct rte_mbuf *
-enic_rxmbuf_alloc(struct rte_mempool *mp)
-{
-	struct rte_mbuf *m;
-
-	m = __rte_mbuf_raw_alloc(mp);
-	__rte_mbuf_sanity_check_raw(m, 0);
-	return m;
-}
-
 void enic_set_hdr_split_size(struct enic *enic, u16 split_hdr_size)
 {
 	vnic_set_hdr_split_size(enic->vdev, split_hdr_size);
@@ -321,7 +311,7 @@ static int enic_rq_alloc_buf(struct vnic_rq *rq)
 	struct rq_enet_desc *desc = vnic_rq_next_desc(rq);
 	uint8_t type = RQ_ENET_TYPE_ONLY_SOP;
 	u16 split_hdr_size = vnic_get_hdr_split_size(enic->vdev);
-	struct rte_mbuf *mbuf = enic_rxmbuf_alloc(rq->mp);
+	struct rte_mbuf *mbuf = rte_mbuf_raw_alloc(rq->mp);
 	struct rte_mbuf *hdr_mbuf = NULL;
 
 	if (!mbuf) {
@@ -334,7 +324,7 @@ static int enic_rq_alloc_buf(struct vnic_rq *rq)
 			rte_mempool_put(mbuf->pool, mbuf);
 			return -1;
 		}
-		hdr_mbuf = enic_rxmbuf_alloc(rq->mp);
+		hdr_mbuf = rte_mbuf_raw_alloc(rq->mp);
 		if (!hdr_mbuf) {
 			rte_mempool_put(mbuf->pool, mbuf);
 			dev_err(enic,