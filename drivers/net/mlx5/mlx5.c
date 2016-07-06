serglelv/dpdk.org

commit cc9216d5dbfa10dda2ad781d36f36a52ca886d2d

net/mlx5: apply DPDK's master difference
based on:
  16dba07133bd ("net/enic: fix Rx scatter with multiple queues")

Not applyed:
 - 1d1bc870265d ("net: fix PCI class id")
 - d1d914ebbc25 ("mempool: allocate in several memory chunks by default")

Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

drivers/net/mlx5/mlx5.c
@@ -69,21 +69,23 @@
 #include "mlx5_autoconf.h"
 #include "mlx5_defs.h"
 
+/* Device parameter to enable RX completion queue compression. */
+#define MLX5_RXQ_CQE_COMP_EN "rxq_cqe_comp_en"
+
 /* Device parameter to configure inline send. */
 #define MLX5_TXQ_INLINE "txq_inline"
 
-/* Device parameter to configure inline send. */
+/* Device parameter to configure new inline send. */
 #define MLX5_TXQ_INLINE_NEW "txq_inline_new"
 
-/* Device parameter to configure the number of TX queues threshold for
- * enabling inline send. */
+/*
+ * Device parameter to configure the number of TX queues threshold for
+ * enabling inline send.
+ */
 #define MLX5_TXQS_MIN_INLINE "txqs_min_inline"
 
 /* Device parameter to enable multi-packet send WQEs. */
 #define MLX5_TXQ_MPW_EN "txq_mpw_en"
-
-/* Device parameter to enable RX completion queue compression. */
-#define MLX5_RXQ_CQE_COMP_EN "rxq_cqe_comp_en"
 
 /**
  * Retrieve integer value from environment variable.
@@ -266,7 +268,7 @@ mlx5_dev_idx(struct rte_pci_addr *pci_addr)
  *   User data.
  *
  * @return
- *   0 on success, errno value on failure.
+ *   0 on success, negative errno value on failure.
  */
 static int
 mlx5_args_check(const char *key, const char *val, void *opaque)
@@ -280,20 +282,19 @@ mlx5_args_check(const char *key, const char *val, void *opaque)
 		WARN("%s: \"%s\" is not a valid integer", key, val);
 		return errno;
 	}
-
-	if (strcmp(MLX5_TXQ_INLINE, key) == 0)
-		priv->txq_inline = tmp;
-	else if(strcmp(MLX5_TXQ_INLINE_NEW, key) == 0)
-		priv->txq_inline_new = tmp;
-	else if (strcmp(MLX5_TXQS_MIN_INLINE, key) == 0)
-		priv->txqs_inline = tmp;
-	else if (strcmp(MLX5_TXQ_MPW_EN, key) == 0)
-		priv->mps = !!tmp;
-	else if (strcmp(MLX5_RXQ_CQE_COMP_EN, key) == 0)
+	if (strcmp(MLX5_RXQ_CQE_COMP_EN, key) == 0) {
 		priv->cqe_comp = !!tmp;
-	else {
+	} else if (strcmp(MLX5_TXQ_INLINE, key) == 0) {
+		priv->txq_inline = tmp;
+	} else if (strcmp(MLX5_TXQS_MIN_INLINE, key) == 0) {
+		priv->txqs_inline = tmp;
+	} else if (strcmp(MLX5_TXQ_MPW_EN, key) == 0) {
+		priv->mps = !!tmp;
+	} else if(strcmp(MLX5_TXQ_INLINE_NEW, key) == 0) {
+		priv->txq_inline_new = tmp;
+	} else {
 		WARN("%s: unknown parameter", key);
-		return EINVAL;
+		return -EINVAL;
 	}
 	return 0;
 }
@@ -312,12 +313,13 @@ mlx5_args_check(const char *key, const char *val, void *opaque)
 static int
 mlx5_args(struct priv *priv, struct rte_devargs *devargs)
 {
-	static const char *params[] = {
+	const char **params = (const char *[]){
+		MLX5_RXQ_CQE_COMP_EN,
 		MLX5_TXQ_INLINE,
 		MLX5_TXQ_INLINE_NEW,
 		MLX5_TXQS_MIN_INLINE,
 		MLX5_TXQ_MPW_EN,
-		MLX5_RXQ_CQE_COMP_EN,
+		NULL,
 	};
 	struct rte_kvargs *kvlist;
 	int ret = 0;
@@ -325,11 +327,12 @@ mlx5_args(struct priv *priv, struct rte_devargs *devargs)
 
 	if (devargs == NULL)
 		return 0;
+	/* Following UGLY cast is done to pass checkpatch. */
 	kvlist = rte_kvargs_parse(devargs->args, params);
 	if (kvlist == NULL)
 		return 0;
 	/* Process parameters. */
-	for (i = 0; (i != RTE_DIM(params)); ++i) {
+	for (i = 0; (params[i] != NULL); ++i) {
 		if (rte_kvargs_count(kvlist, params[i])) {
 			ret = rte_kvargs_process(kvlist, params[i],
 						 mlx5_args_check, priv);
@@ -512,6 +515,7 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		priv->port = port;
 		priv->pd = pd;
 		priv->mtu = ETHER_MTU;
+		priv->mps = mps; /* Enable MPW by default if supported. */
 		priv->cqe_comp = 1; /* Enable compression by default. */
 		priv->mps = mps; /* Enable MPW by default if supported */
 		err = mlx5_args(priv, pci_dev->devargs);

drivers/net/mlx5/Makefile
@@ -54,12 +54,15 @@ DEPDIRS-$(CONFIG_RTE_LIBRTE_MLX5_PMD) += lib/librte_ether
 DEPDIRS-$(CONFIG_RTE_LIBRTE_MLX5_PMD) += lib/librte_mbuf
 DEPDIRS-$(CONFIG_RTE_LIBRTE_MLX5_PMD) += lib/librte_eal
 DEPDIRS-$(CONFIG_RTE_LIBRTE_MLX5_PMD) += lib/librte_mempool
+DEPDIRS-$(CONFIG_RTE_LIBRTE_MLX5_PMD) += lib/librte_kvargs
 
 # Basic CFLAGS.
 CFLAGS += -O3
 CFLAGS += -std=gnu99 -Wall -Wextra
 CFLAGS += -g
 CFLAGS += -I.
+CFLAGS += -D_BSD_SOURCE
+CFLAGS += -D_DEFAULT_SOURCE
 CFLAGS += -D_XOPEN_SOURCE=600
 CFLAGS += $(WERROR_FLAGS)
 CFLAGS += -Wno-strict-prototypes
@@ -99,7 +102,9 @@ ifndef V
 AUTOCONF_OUTPUT := >/dev/null
 endif
 
-mlx5_autoconf.h: $(RTE_SDK)/scripts/auto-config-h.sh
+mlx5_autoconf.h.new: FORCE
+
+mlx5_autoconf.h.new: $(RTE_SDK)/scripts/auto-config-h.sh
 	$Q $(RM) -f -- '$@'
 	$Q sh -- '$<' '$@' \
 		HAVE_VERBS_IBV_EXP_CQ_COMPRESSED_CQE \
@@ -112,9 +117,16 @@ mlx5_autoconf.h: $(RTE_SDK)/scripts/auto-config-h.sh
 		enum MLX5_ETH_VLAN_INLINE_HEADER_SIZE \
 		$(AUTOCONF_OUTPUT)
 
+# Create mlx5_autoconf.h or update it in case it differs from the new one.
+
+mlx5_autoconf.h: mlx5_autoconf.h.new
+	$Q [ -f '$@' ] && \
+		cmp '$<' '$@' $(AUTOCONF_OUTPUT) || \
+		mv '$<' '$@'
+
 $(SRCS-$(CONFIG_RTE_LIBRTE_MLX5_PMD):.c=.o): mlx5_autoconf.h
 
 clean_mlx5: FORCE
-	$Q rm -f -- mlx5_autoconf.h
+	$Q rm -f -- mlx5_autoconf.h mlx5_autoconf.h.new
 
 clean: clean_mlx5

drivers/net/mlx5/mlx5.h
@@ -39,7 +39,6 @@
 #include <limits.h>
 #include <net/if.h>
 #include <netinet/in.h>
-#include <linux/if.h>
 
 /* Verbs header. */
 /* ISO C doesn't support unnamed structs/unions, disabling -pedantic. */

drivers/net/mlx5/mlx5_ethdev.c
@@ -44,7 +44,6 @@
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <linux/if.h>
 #include <linux/ethtool.h>
 #include <linux/sockios.h>
 #include <fcntl.h>
@@ -657,7 +656,7 @@ mlx5_link_update_unlocked(struct rte_eth_dev *dev, int wait_to_complete)
 	memset(&dev_link, 0, sizeof(dev_link));
 	dev_link.link_status = ((ifr.ifr_flags & IFF_UP) &&
 				(ifr.ifr_flags & IFF_RUNNING));
-	ifr.ifr_data = &edata;
+	ifr.ifr_data = (void *)&edata;
 	if (priv_ifreq(priv, SIOCETHTOOL, &ifr)) {
 		WARN("ioctl(SIOCETHTOOL, ETHTOOL_GSET) failed: %s",
 		     strerror(errno));
@@ -762,17 +761,21 @@ mlx5_dev_set_mtu(struct rte_eth_dev *dev, uint16_t mtu)
 			continue;
 		mb_len = rte_pktmbuf_data_room_size(rxq->mp);
 		assert(mb_len >= RTE_PKTMBUF_HEADROOM);
-		/* Determine the number of SGEs needed for a full packet
-		 * and round it to the next power of two. */
+		/*
+		 * Determine the number of SGEs needed for a full packet
+		 * and round it to the next power of two.
+		 */
 		sges_n = log2above((size / mb_len) + !!(size % mb_len));
 		if (sges_n != rxq->sges_n)
 			break;
 	}
-	/* If all queues have the right number of SGEs, a simple rehash
+	/*
+	 * If all queues have the right number of SGEs, a simple rehash
 	 * of their buffers is enough, otherwise SGE information can only
 	 * be updated in a queue by recreating it. All resources that depend
 	 * on queues (flows, indirection tables) must be recreated as well in
-	 * that case. */
+	 * that case.
+	 */
 	rehash = (i == priv->rxqs_n);
 	if (!rehash) {
 		/* Clean up everything as with mlx5_dev_stop(). */
@@ -817,8 +820,10 @@ recover:
 		/* Double fault, disable RX. */
 		break;
 	}
-	/* Use a safe RX burst function in case of error, otherwise mimic
-	 * mlx5_dev_start(). */
+	/*
+	 * Use a safe RX burst function in case of error, otherwise mimic
+	 * mlx5_dev_start().
+	 */
 	if (ret) {
 		ERROR("unable to reconfigure RX queues, RX disabled");
 		rx_func = removed_rx_burst;
@@ -864,7 +869,7 @@ mlx5_dev_get_flow_ctrl(struct rte_eth_dev *dev, struct rte_eth_fc_conf *fc_conf)
 	if (mlx5_is_secondary())
 		return -E_RTE_SECONDARY;
 
-	ifr.ifr_data = &ethpause;
+	ifr.ifr_data = (void *)&ethpause;
 	priv_lock(priv);
 	if (priv_ifreq(priv, SIOCETHTOOL, &ifr)) {
 		ret = errno;
@@ -915,7 +920,7 @@ mlx5_dev_set_flow_ctrl(struct rte_eth_dev *dev, struct rte_eth_fc_conf *fc_conf)
 	if (mlx5_is_secondary())
 		return -E_RTE_SECONDARY;
 
-	ifr.ifr_data = &ethpause;
+	ifr.ifr_data = (void *)&ethpause;
 	ethpause.autoneg = fc_conf->autoneg;
 	if (((fc_conf->mode & RTE_FC_FULL) == RTE_FC_FULL) ||
 	    (fc_conf->mode & RTE_FC_RX_PAUSE))
@@ -1109,7 +1114,7 @@ priv_dev_interrupt_handler_uninstall(struct priv *priv, struct rte_eth_dev *dev)
 		rte_eal_alarm_cancel(mlx5_dev_link_status_handler, dev);
 	priv->pending_alarm = 0;
 	priv->intr_handle.fd = 0;
-	priv->intr_handle.type = 0;
+	priv->intr_handle.type = RTE_INTR_HANDLE_UNKNOWN;
 }
 
 /**
@@ -1311,7 +1316,8 @@ mlx5_secondary_data_setup(struct priv *priv)
 					   primary_txq->elts_n,
 					   primary_txq_ctrl->socket,
 					   NULL) == 0) {
-				txq_ctrl->txq.stats.idx = primary_txq->stats.idx;
+				txq_ctrl->txq.stats.idx =
+					primary_txq->stats.idx;
 				tx_queues[i] = &txq_ctrl->txq;
 				continue;
 			}

drivers/net/mlx5/mlx5_mac.c
@@ -38,7 +38,6 @@
 #include <inttypes.h>
 #include <errno.h>
 #include <netinet/in.h>
-#include <linux/if.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
 

drivers/net/mlx5/mlx5_rxmode.c
@@ -347,7 +347,9 @@ priv_special_flow_enable_all(struct priv *priv)
 {
 	enum hash_rxq_flow_type flow_type;
 
-	for (flow_type = 0; flow_type != HASH_RXQ_FLOW_TYPE_MAC; ++flow_type) {
+	for (flow_type = HASH_RXQ_FLOW_TYPE_PROMISC;
+			flow_type != HASH_RXQ_FLOW_TYPE_MAC;
+			++flow_type) {
 		int ret;
 
 		ret = priv_special_flow_enable(priv, flow_type);
@@ -372,7 +374,9 @@ priv_special_flow_disable_all(struct priv *priv)
 {
 	enum hash_rxq_flow_type flow_type;
 
-	for (flow_type = 0; flow_type != HASH_RXQ_FLOW_TYPE_MAC; ++flow_type)
+	for (flow_type = HASH_RXQ_FLOW_TYPE_PROMISC;
+			flow_type != HASH_RXQ_FLOW_TYPE_MAC;
+			++flow_type)
 		priv_special_flow_disable(priv, flow_type);
 }
 

drivers/net/mlx5/mlx5_rxq.c
@@ -269,7 +269,7 @@ priv_flow_attr(struct priv *priv, struct ibv_exp_flow_attr *flow_attr,
 static enum hash_rxq_type
 hash_rxq_type_from_pos(const struct ind_table_init *table, unsigned int pos)
 {
-	enum hash_rxq_type type = 0;
+	enum hash_rxq_type type = HASH_RXQ_TCPV4;
 
 	assert(pos < table->hash_types_n);
 	do {
@@ -609,9 +609,11 @@ priv_allow_flow_type(struct priv *priv, enum hash_rxq_flow_type type)
 int
 priv_rehash_flows(struct priv *priv)
 {
-	unsigned int i;
+	enum hash_rxq_flow_type i;
 
-	for (i = 0; (i != RTE_DIM((*priv->hash_rxqs)[0].special_flow)); ++i)
+	for (i = HASH_RXQ_FLOW_TYPE_PROMISC;
+			i != RTE_DIM((*priv->hash_rxqs)[0].special_flow);
+			++i)
 		if (!priv_allow_flow_type(priv, i)) {
 			priv_special_flow_disable(priv, i);
 		} else {
@@ -871,6 +873,11 @@ rxq_setup(struct rxq_ctrl *tmpl)
 	struct rte_mbuf *(*elts)[tmpl->rxq.elts_n] =
 		rte_calloc_socket("RXQ", 1, sizeof(*elts), 0, tmpl->socket);
 
+	if (cq->cqe_sz != RTE_CACHE_LINE_SIZE) {
+		ERROR("Wrong MLX5_CQE_SIZE environment variable value: "
+		      "it should be set to %u", RTE_CACHE_LINE_SIZE);
+		return EINVAL;
+	}
 	if (elts == NULL)
 		return ENOMEM;
 	if (cq->cqe_sz != RTE_CACHE_LINE_SIZE) {
@@ -936,8 +943,8 @@ rxq_ctrl_setup(struct rte_eth_dev *dev, struct rxq_ctrl *rxq_ctrl,
 	} attr;
 	enum ibv_exp_query_intf_status status;
 	unsigned int mb_len = rte_pktmbuf_data_room_size(mp);
-	struct rte_mbuf *(*elts)[desc] = NULL;
 	unsigned int cqe_n = desc - 1;
+	struct rte_mbuf *(*elts)[desc] = NULL;
 	int ret = 0;
 
 	(void)conf; /* Thresholds configuration (ignored). */
@@ -951,8 +958,10 @@ rxq_ctrl_setup(struct rte_eth_dev *dev, struct rxq_ctrl *rxq_ctrl,
 			dev->data->dev_conf.rxmode.max_rx_pkt_len;
 		unsigned int sges_n;
 
-		/* Determine the number of SGEs needed for a full packet
-		 * and round it to the next power of two. */
+		/*
+		 * Determine the number of SGEs needed for a full packet
+		 * and round it to the next power of two.
+		 */
 		sges_n = log2above((size / mb_len) + !!(size % mb_len));
 		tmpl.rxq.sges_n = sges_n;
 		/* Make sure rxq.sges_n did not overflow. */
@@ -981,7 +990,8 @@ rxq_ctrl_setup(struct rte_eth_dev *dev, struct rxq_ctrl *rxq_ctrl,
 	if (priv->hw_csum)
 		tmpl.rxq.csum = !!dev->data->dev_conf.rxmode.hw_ip_checksum;
 	if (priv->hw_csum_l2tun)
-		tmpl.rxq.csum_l2tun = !!dev->data->dev_conf.rxmode.hw_ip_checksum;
+		tmpl.rxq.csum_l2tun =
+			!!dev->data->dev_conf.rxmode.hw_ip_checksum;
 	/* Use the entire RX mempool as the memory region. */
 	tmpl.mr = mlx5_mp2mr(priv->pd, mp);
 	if (tmpl.mr == NULL) {
@@ -1085,8 +1095,10 @@ rxq_ctrl_setup(struct rte_eth_dev *dev, struct rxq_ctrl *rxq_ctrl,
 		      (void *)dev, strerror(ret));
 		goto error;
 	}
-	/* Make sure number of WRs*SGEs match expectations since a queue
-	 * cannot allocate more than "desc" buffers. */
+	/*
+	 * Make sure number of WRs*SGEs match expectations since a queue
+	 * cannot allocate more than "desc" buffers.
+	 */
 	if (((int)attr.wq.max_recv_wr != (desc >> tmpl.rxq.sges_n)) ||
 	    ((int)attr.wq.max_recv_sge != (1 << tmpl.rxq.sges_n))) {
 		ERROR("%p: requested %u*%u but got %u*%u WRs*SGEs",

drivers/net/mlx5/mlx5_rxtx.c
@@ -89,8 +89,10 @@ check_cqe64_seen(volatile struct mlx5_cqe64 *cqe)
 	unsigned int i;
 
 	for (i = 0; i < sizeof(magic) && i < sizeof(*buf); ++i)
-		if (!ret || !(ret = ((*buf)[i] == magic[i])))
+		if (!ret || (*buf)[i] != magic[i]) {
+			ret = 0;
 			(*buf)[i] = magic[i];
+		}
 	return ret;
 }
 
@@ -571,12 +573,12 @@ mlx5_wqe_write_inline_vlan(struct txq *txq, volatile union mlx5_wqe *wqe,
 	 * Copy 4 bytes of VLAN.
 	 * Copy 2 bytes of Ether type.
 	 */
-	rte_memcpy((uint8_t*)(uintptr_t)wqe->inl.eseg.inline_hdr_start,
-		   (uint8_t*)addr, 12);
-	rte_memcpy((uint8_t*)(uintptr_t)wqe->inl.eseg.inline_hdr_start + 12,
+	rte_memcpy((uint8_t *)(uintptr_t)wqe->inl.eseg.inline_hdr_start,
+		   (uint8_t *)addr, 12);
+	rte_memcpy((uint8_t *)(uintptr_t)wqe->inl.eseg.inline_hdr_start + 12,
 		   &vlan, sizeof(vlan));
-	rte_memcpy((uint8_t*)(uintptr_t)wqe->inl.eseg.inline_hdr_start + 16,
-		   ((uint8_t*)addr + 12), 2);
+	rte_memcpy((uint8_t *)(uintptr_t)wqe->inl.eseg.inline_hdr_start + 16,
+		   ((uint8_t *)addr + 12), 2);
 	addr += MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
 	length -= MLX5_ETH_VLAN_INLINE_HEADER_SIZE - sizeof(vlan);
 	size = (sizeof(wqe->inl.ctrl.ctrl) +
@@ -719,8 +721,10 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		volatile struct mlx5_wqe_data_seg *dseg;
 		unsigned int ds = sizeof(*wqe) / 16;
 
-		/* Make sure there is enough room to store this packet and
-		 * that one ring entry remains unused. */
+		/*
+		 * Make sure there is enough room to store this packet and
+		 * that one ring entry remains unused.
+		 */
 		assert(segs_n);
 		if (max < segs_n + 1)
 			break;
@@ -754,12 +758,15 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 			wqe->wqe.eseg.cs_flags =
 				MLX5_ETH_WQE_L3_CSUM |
 				MLX5_ETH_WQE_L4_CSUM;
-		} else
+		} else {
 			wqe->wqe.eseg.cs_flags = 0;
+		}
 		while (--segs_n) {
-			/* Spill on next WQE when the current one does not have
+			/*
+			 * Spill on next WQE when the current one does not have
 			 * enough room left. Size of WQE must a be a multiple
-			 * of data segment size. */
+			 * of data segment size.
+			 */
 			assert(!(sizeof(*wqe) % sizeof(*dseg)));
 			if (!(ds % (sizeof(*wqe) / 16)))
 				dseg = (volatile void *)
@@ -803,8 +810,9 @@ mlx5_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		/* Save elts_head in unused "immediate" field of WQE. */
 		wqe->wqe.ctrl.data[3] = elts_head;
 		txq->elts_comp = 0;
-	} else
+	} else {
 		txq->elts_comp = comp;
+	}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 	/* Increment sent packets counter. */
 	txq->stats.opackets += i;
@@ -862,8 +870,10 @@ mlx5_tx_burst_inline(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		volatile struct mlx5_wqe_data_seg *dseg;
 		unsigned int ds = sizeof(*wqe) / 16;
 
-		/* Make sure there is enough room to store this packet and
-		 * that one ring entry remains unused. */
+		/*
+		 * Make sure there is enough room to store this packet and
+		 * that one ring entry remains unused.
+		 */
 		assert(segs_n);
 		if (max < segs_n + 1)
 			break;
@@ -882,8 +892,9 @@ mlx5_tx_burst_inline(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 			wqe->inl.eseg.cs_flags =
 				MLX5_ETH_WQE_L3_CSUM |
 				MLX5_ETH_WQE_L4_CSUM;
-		} else
+		} else {
 			wqe->inl.eseg.cs_flags = 0;
+		}
 		/* Retrieve buffer information. */
 		addr = rte_pktmbuf_mtod(buf, uintptr_t);
 		length = DATA_LEN(buf);
@@ -911,9 +922,11 @@ mlx5_tx_burst_inline(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 				mlx5_wqe_write(txq, wqe, addr, length, lkey);
 		}
 		while (--segs_n) {
-			/* Spill on next WQE when the current one does not have
+			/*
+			 * Spill on next WQE when the current one does not have
 			 * enough room left. Size of WQE must a be a multiple
-			 * of data segment size. */
+			 * of data segment size.
+			 */
 			assert(!(sizeof(*wqe) % sizeof(*dseg)));
 			if (!(ds % (sizeof(*wqe) / 16)))
 				dseg = (volatile void *)
@@ -938,7 +951,7 @@ mlx5_tx_burst_inline(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		/* Update DS field in WQE. */
 		wqe->inl.ctrl.data[1] &= htonl(0xffffffc0);
 		wqe->inl.ctrl.data[1] |= htonl(ds & 0x3f);
-	skip_segs:
+skip_segs:
 		elts_head = elts_head_next;
 #ifdef MLX5_PMD_SOFT_COUNTERS
 		/* Increment sent bytes counter. */
@@ -957,8 +970,9 @@ mlx5_tx_burst_inline(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		/* Save elts_head in unused "immediate" field of WQE. */
 		wqe->inl.ctrl.data[3] = elts_head;
 		txq->elts_comp = 0;
-	} else
+	} else {
 		txq->elts_comp = comp;
+	}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 	/* Increment sent packets counter. */
 	txq->stats.opackets += i;
@@ -1176,8 +1190,10 @@ mlx5_mpw_close(struct txq *txq, struct mlx5_mpw *mpw)
 {
 	unsigned int num = mpw->pkts_n;
 
-	/* Store size in multiple of 16 bytes. Control and Ethernet segments
-	 * count as 2. */
+	/*
+	 * Store size in multiple of 16 bytes. Control and Ethernet segments
+	 * count as 2.
+	 */
 	mpw->wqe->mpw.ctrl.data[1] = htonl(txq->qp_num_8s | (2 + num));
 	mpw->state = MLX5_MPW_STATE_CLOSED;
 	if (num < 3)
@@ -1233,8 +1249,10 @@ mlx5_tx_burst_mpw(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		unsigned int segs_n = buf->nb_segs;
 		uint32_t cs_flags = 0;
 
-		/* Make sure there is enough room to store this packet and
-		 * that one ring entry remains unused. */
+		/*
+		 * Make sure there is enough room to store this packet and
+		 * that one ring entry remains unused.
+		 */
 		assert(segs_n);
 		if (max < segs_n + 1)
 			break;
@@ -1311,8 +1329,9 @@ mlx5_tx_burst_mpw(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		/* Save elts_head in unused "immediate" field of WQE. */
 		wqe->mpw.ctrl.data[3] = elts_head;
 		txq->elts_comp = 0;
-	} else
+	} else {
 		txq->elts_comp = comp;
+	}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 	/* Increment sent packets counter. */
 	txq->stats.opackets += i;
@@ -1373,8 +1392,10 @@ mlx5_mpw_inline_close(struct txq *txq, struct mlx5_mpw *mpw)
 	unsigned int size;
 
 	size = sizeof(*mpw->wqe) - MLX5_MWQE64_INL_DATA + mpw->total_len;
-	/* Store size in multiple of 16 bytes. Control and Ethernet segments
-	 * count as 2. */
+	/*
+	 * Store size in multiple of 16 bytes. Control and Ethernet segments
+	 * count as 2.
+	 */
 	mpw->wqe->mpw_inl.ctrl.data[1] =
 		htonl(txq->qp_num_8s | ((size + 15) / 16));
 	mpw->state = MLX5_MPW_STATE_CLOSED;
@@ -1430,8 +1451,10 @@ mlx5_tx_burst_mpw_inline(void *dpdk_txq, struct rte_mbuf **pkts,
 		unsigned int segs_n = buf->nb_segs;
 		uint32_t cs_flags = 0;
 
-		/* Make sure there is enough room to store this packet and
-		 * that one ring entry remains unused. */
+		/*
+		 * Make sure there is enough room to store this packet and
+		 * that one ring entry remains unused.
+		 */
 		assert(segs_n);
 		if (max < segs_n + 1)
 			break;
@@ -1540,8 +1563,9 @@ mlx5_tx_burst_mpw_inline(void *dpdk_txq, struct rte_mbuf **pkts,
 			if (mpw.pkts_n == MLX5_MPW_DSEG_MAX) {
 				mlx5_mpw_inline_close(txq, &mpw);
 				inline_room = txq->max_inline;
-			} else
+			} else {
 				inline_room -= length;
+			}
 		}
 		mpw.total_len += length;
 		elts_head = elts_head_next;
@@ -1565,8 +1589,9 @@ mlx5_tx_burst_mpw_inline(void *dpdk_txq, struct rte_mbuf **pkts,
 		/* Save elts_head in unused "immediate" field of WQE. */
 		wqe->mpw_inl.ctrl.data[3] = elts_head;
 		txq->elts_comp = 0;
-	} else
+	} else {
 		txq->elts_comp = comp;
+	}
 #ifdef MLX5_PMD_SOFT_COUNTERS
 	/* Increment sent packets counter. */
 	txq->stats.opackets += i;
@@ -1654,9 +1679,11 @@ mlx5_rx_poll_len(struct rxq *rxq, volatile struct mlx5_cqe64 *cqe,
 
 		len = ntohl((*mc)[zip->ai & 7].byte_cnt);
 		if ((++zip->ai & 7) == 0) {
-			/* Increment consumer index to skip the number of
+			/*
+			 * Increment consumer index to skip the number of
 			 * CQEs consumed. Hardware leaves holes in the CQ
-			 * ring for software use. */
+			 * ring for software use.
+			 */
 			zip->ca = zip->na;
 			zip->na += 8;
 		}
@@ -1685,9 +1712,8 @@ mlx5_rx_poll_len(struct rxq *rxq, volatile struct mlx5_cqe64 *cqe,
 		if (MLX5_CQE_FORMAT(op_own) == MLX5_COMPRESSED) {
 			volatile struct mlx5_mini_cqe8 (*mc)[8] =
 				(volatile struct mlx5_mini_cqe8 (*)[8])
-				(uintptr_t)&(*rxq->cqes)[rxq->cq_ci &
-							 cqe_cnt].cqe64;
-
+				(uintptr_t)(&(*rxq->cqes)[rxq->cq_ci &
+							  cqe_cnt].cqe64);
 			/* Fix endianness. */
 			zip->cqe_cnt = ntohl(cqe->byte_cnt);
 			/*
@@ -1706,8 +1732,9 @@ mlx5_rx_poll_len(struct rxq *rxq, volatile struct mlx5_cqe64 *cqe,
 			/* Get packet size to return. */
 			len = ntohl((*mc)[0].byte_cnt);
 			zip->ai = 1;
-		} else
+		} else {
 			len = ntohl(cqe->byte_cnt);
+		}
 		/* Error while receiving packet. */
 		if (unlikely(MLX5_CQE_OPCODE(op_own) == MLX5_CQE_RESP_ERR))
 			return -1;
@@ -1784,11 +1811,11 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 	const unsigned int sges_n = rxq->sges_n;
 	struct rte_mbuf *pkt = NULL;
 	struct rte_mbuf *seg = NULL;
+	volatile struct mlx5_cqe64 *cqe =
+		&(*rxq->cqes)[rxq->cq_ci & cqe_cnt].cqe64;
 	unsigned int i = 0;
 	unsigned int rq_ci = rxq->rq_ci << sges_n;
 	int len;
-	volatile struct mlx5_cqe64 *cqe =
-		     &(*rxq->cqes)[rxq->cq_ci & cqe_cnt].cqe64;
 
 	while (pkts_n) {
 		unsigned int idx = rq_ci & wqe_cnt;
@@ -1852,9 +1879,11 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		PORT(rep) = PORT(seg);
 		NEXT(rep) = NULL;
 		(*rxq->elts)[idx] = rep;
-		/* Fill NIC descriptor with the new buffer.  The lkey and size
+		/*
+		 * Fill NIC descriptor with the new buffer.  The lkey and size
 		 * of the buffers are already known, only the buffer address
-		 * changes. */
+		 * changes.
+		 */
 		wqe->addr = htonll(rte_pktmbuf_mtod(rep, uintptr_t));
 		if (len > DATA_LEN(seg)) {
 			len -= DATA_LEN(seg);
@@ -1872,7 +1901,7 @@ mlx5_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
 		pkt = NULL;
 		--pkts_n;
 		++i;
-	skip:
+skip:
 		/* Align consumer index to the next stride. */
 		rq_ci >>= sges_n;
 		++rq_ci;

commit 771e5176746683503aee2bf120adc2695a09753e

net/mlx5: fix redundant statement
Fixes: cc9216d5dbfa ("net/mlx5: apply DPDK's master difference")

Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

drivers/net/mlx5/mlx5.c
@@ -517,7 +517,6 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		priv->mtu = ETHER_MTU;
 		priv->mps = mps; /* Enable MPW by default if supported. */
 		priv->cqe_comp = 1; /* Enable compression by default. */
-		priv->mps = mps; /* Enable MPW by default if supported */
 		err = mlx5_args(priv, pci_dev->devargs);
 		if (err) {
 			ERROR("failed to process device arguments: %s",
771e5176746683503aee2bf120adc2695a09753e
Adrien Mazarguil <adrien.mazarguil@6wind.com>
2016-07-06 16:04:44 +0200

    net/mlx5: fix redundant statement
    Fixes: cc9216d5dbfa ("net/mlx5: apply DPDK's master difference")
    
    Signed-off-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>

--- drivers/net/mlx5/mlx5.c
+++ drivers/net/mlx5/mlx5.c
@@ -517,7 +517,6 @@ mlx5_pci_devinit(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 		priv->mtu = ETHER_MTU;
 		priv->mps = mps; /* Enable MPW by default if supported. */
 		priv->cqe_comp = 1; /* Enable compression by default. */
-		priv->mps = mps; /* Enable MPW by default if supported */
 		err = mlx5_args(priv, pci_dev->devargs);
 		if (err) {
 			ERROR("failed to process device arguments: %s",
